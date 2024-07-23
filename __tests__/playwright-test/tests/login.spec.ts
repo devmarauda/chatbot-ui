aa657b43365c9fffb0bf6c9bc8721888ec7b9002
devmarauda <dev@tmgllc.io>
2024-07-24 01:58:19 +1000

    dev

--- __tests__/playwright-test/tests/login.spec.ts
+++ __tests__/playwright-test/tests/login.spec.ts
@@ -1,13 +1,14 @@
 import { test, expect } from '@playwright/test';
+import type { Page } from '@playwright/test';
 
-test('start chatting is displayed', async ({ page }) => {
+test('start chatting is displayed', async ({ page }: { page: Page }) => {
   await page.goto('http://localhost:3000/');
 
   //expect the start chatting link to be visible
-  await expect (page.getByRole('link', { name: 'Start Chatting' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Start Chatting' })).toBeVisible();
 });
 
-test('No password error message', async ({ page }) => {
+test('No password error message', async ({ page }: { page: Page }) => {
   await page.goto('http://localhost:3000/login');
   //fill in dummy email
   await page.getByPlaceholder('you@example.com').fill('dummyemail@gmail.com');
@@ -18,7 +19,7 @@ test('No password error message', async ({ page }) => {
   await expect(page.getByText('Invalid login credentials')).toBeVisible();
   
 });
-test('No password for signup', async ({ page }) => {
+test('No password for signup', async ({ page }: { page: Page }) => {
   await page.goto('http://localhost:3000/login');
   
   await page.getByPlaceholder('you@example.com').fill('dummyEmail@Gmail.com');
@@ -26,7 +27,7 @@ test('No password for signup', async ({ page }) => {
   //validate appropriate error is thrown for missing password when signing up
   await expect(page.getByText('Signup requires a valid')).toBeVisible();
 });
-test('invalid username for signup', async ({ page }) => {
+test('invalid username for signup', async ({ page }: { page: Page }) => {
   await page.goto('http://localhost:3000/login');
   
   await page.getByPlaceholder('you@example.com').fill('dummyEmail');
@@ -35,7 +36,7 @@ test('invalid username for signup', async ({ page }) => {
   //validate appropriate error is thrown for invalid username when signing up
   await expect(page.getByText('Unable to validate email')).toBeVisible();
 });
-test('password reset message', async ({ page }) => {
+test('password reset message', async ({ page }: { page: Page }) => {
   await page.goto('http://localhost:3000/login');
   await page.getByPlaceholder('you@example.com').fill('demo@gmail.com');
   await page.getByRole('button', { name: 'Reset' }).click();
